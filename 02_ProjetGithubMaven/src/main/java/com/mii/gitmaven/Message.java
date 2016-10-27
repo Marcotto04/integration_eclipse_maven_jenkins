7d44bd5562f2cc2fffeef0031418a7d0c124abe5
Marcotto04 <megama_1@hotmail.fr>
2016-10-27 11:34:34 +0200

    Erreur indice

--- 02_ProjetGithubMaven/src/main/java/com/mii/gitmaven/Message.java
+++ 02_ProjetGithubMaven/src/main/java/com/mii/gitmaven/Message.java
@@ -15,7 +15,7 @@ public class Message {
 	// choisi un message de manière aléatoire et le peronnaliser
 	public String getMessage(String nom){
 		Random rnd = new Random();
-		String msg = tbMessage[rnd.nextInt(tbMessage.length)-1];
+		String msg = tbMessage[rnd.nextInt(tbMessage.length)];
 		return msg.replace("%%", nom);
 	}
 }
